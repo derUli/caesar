12768b7d181216fd8cd75f70a136a94e3505e2c0
Ulrich Schmidt <admin@deruli.de>
2017-09-28 07:48:09 +0200

    zwischenstand

---
+++ src/content/modules/caesar/templates/footer.php
@@ -0,0 +1,7 @@
+<script type="text/javascript"
+	src="<?php echo ModuleHelper::buildModuleRessourcePath("rot13", "js/rot13.js")?>"></script>
+<style type="text/css">
+.rot13 {
+	cursor: help;
+}
+</style>
